218ef6e2e8501387d2c87afdbcf3790c2143bc74
Constantine <50499405@mail.ru>
2018-11-11 23:47:19 +0300

    [Minor] Removed extra code

--- js/utils/scrollUtils.js
+++ js/utils/scrollUtils.js
@@ -1,9 +1,8 @@
 const scrollUtils = {
   nodeInViewport(node, offset = 0) {
     const nodeTopX = node.getBoundingClientRect().top;
-    const screenBottomX = window.innerHeight + window.pageXOffset;
 
-    const intersectionX = nodeTopX - screenBottomX - offset;
+    const intersectionX = nodeTopX - window.innerHeight - offset;
 
     return intersectionX <= 0;
   }
